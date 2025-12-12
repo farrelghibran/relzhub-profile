f75432064f7139abf195733a4056e5ffab2b45e0
Farrel Ghibran <farrelghibran>
2025-12-12 19:40:48 +0700

    aowkawok

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,46 +1,46 @@
-import { PrismaClient, Prisma } from "@prisma/client";
+// import { PrismaClient, Prisma } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-const statusData: Prisma.StatusUncheckedCreateInput[] = [
-  {
-    title: "Active",
-    color: "green",
-    Game: {
-      create: [
-        {
-          title: "Fish It",
-          content: "Improve your auto farm with relzhub fishit",
-          image:
-            "https://tr.rbxcdn.com/180DAY-8d479799a34addd0e58d0ddb53196d78/768/432/Image/Webp/noFilter",
-        },
-      ],
-    },
-  },
-  {
-    title: "Update",
-    color: "yellow",
-  },
-  {
-    title: "Discontinue",
-    color: "red",
-  },
-];
-const relzhubData: Prisma.RelzhubUncheckedCreateInput[] = [
-  {
-    loader: `loadstring(game:HttpGet("https://relzhub.farrelghibran.com/loader.lua"))`,
-    discord_url: "discord.gg/relzhub",
-    max_steps: 3,
-  },
-];
+// const statusData: Prisma.StatusCreateInput[] = [
+//   {
+//     title: "Active",
+//     color: "green",
+//     Game: {
+//       create: [
+//         {
+//           title: "Fish It",
+//           content: "Improve your auto farm with relzhub fishit",
+//           image:
+//             "https://tr.rbxcdn.com/180DAY-8d479799a34addd0e58d0ddb53196d78/768/432/Image/Webp/noFilter",
+//         },
+//       ],
+//     },
+//   },
+//   {
+//     title: "Update",
+//     color: "yellow",
+//   },
+//   {
+//     title: "Discontinue",
+//     color: "red",
+//   },
+// ];
+// const relzhubData: Prisma.RelzhubCreateInput[] = [
+//   {
+//     loader: `loadstring(game:HttpGet("https://relzhub.farrelghibran.com/loader.lua"))`,
+//     discord_url: "discord.gg/relzhub",
+//     max_steps: 3,
+//   },
+// ];
 
-export async function main() {
-  for (const u of statusData) {
-    await prisma.status.create({ data: u });
-  }
-  for (const u of relzhubData) {
-    await prisma.relzhub.create({ data: u });
-  }
-}
+// export async function main() {
+//   for (const u of statusData) {
+//     await prisma.status.create({ data: u });
+//   }
+//   for (const u of relzhubData) {
+//     await prisma.relzhub.create({ data: u });
+//   }
+// }
 
-main();
+// main();
