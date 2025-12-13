22549687c6b3b90d9175b5a8c6c98e8f9d1d8a45
Farrel Ghibran <farrelghibran>
2025-12-13 20:14:50 +0700

    new update

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 import "dotenv/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+
+  datasource: {
+    url: process.env.DATABASE_URL!,
+  },
+
   migrations: {
     path: "prisma/migrations",
-    seed: `tsx prisma/seed.ts`,
-  },
-  engine: "classic",
-  datasource: {
-    url: env("DATABASE_URL"),
+    seed: "tsx prisma/seed.ts",
   },
 });
